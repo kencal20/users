4b4a1a9978b86a06db5db0ae6461a474f7ce68e0
kenneth adjei-mensah <kenadjei20@gmail.com>
2021-09-28 12:27:04 +0000

    user actions

---
+++ src/actions/userActions.js
@@ -0,0 +1,51 @@
+import { getFirestore } from "redux-firestore";
+
+export const addUser = (newUser) => {
+  // newUser.id = Math.random().toString();
+  return (dispatch, state, { getFirestore }) => {
+    getFirestore()
+      .collection("users")
+      .add(newUser)
+      .then((docs) => {
+        console.log(docs);
+      });
+  };
+  // return{
+  //    type: "ADD_USER",
+  //   payload: newUser,
+  // }
+};
+
+export const editUser = (updatedUser) => {
+  return(dispatch,state,{getFirestore})=>{
+        getFirestore().collection("users").doc(updatedUser.id).set(updatedUser).then(()=>{})
+   
+  };};
+
+
+
+
+export const deleteUser = (userId) => {
+  return (dispatch,state,{getFirestore})=>{
+        getFirestore().collection("users").doc(userId).delete().then(()=>{})
+    
+  };
+};
+
+export const getAllUsers = () => {
+  return (dispatch, state, { getFirestore }) => {
+    getFirestore()
+      .collection("users")
+      .onSnapshot((snapshot) => {
+        let users = [];
+        snapshot.forEach((doc) => {
+          users.push(doc.data());
+        });
+
+        dispatch({
+          type: "SET_ALL_USERS",
+          payload: users,
+        });
+      });
+  };
+};

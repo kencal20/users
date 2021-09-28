03a3b2f46e609ff7b03314aae8c3c1d8d25f5d65
kenneth adjei-mensah <kenadjei20@gmail.com>
2021-09-28 12:24:21 +0000

    edit user

---
+++ src/Components/EditForm.jsx
@@ -0,0 +1,66 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { editUser } from '../actions/userActions';
+
+
+class EditForm extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+             name : props.user.name,
+             email : props.user.email,
+             gen : props.user.gen,
+             id:props.user.id
+        }
+    }
+
+
+    handleChange = (e)=>{
+        this.setState({ [e.target.name] : e.target.value})
+    };
+
+    handleSubmit = (e)=>{
+        e.preventDefault()
+        this.props.editUser(this.state)
+
+
+        this.setState({
+            name : '',
+            email : '',
+            gen : ''
+        });
+        this.props.closeModal();
+        
+    }
+
+
+
+    render() {
+        return (
+            <form onSubmit={this.handleSubmit} className='form'>
+               <div className="name">
+                  <label htmlFor="name">Name</label><br/>
+                  <input type="text" name="name" value={this.state.name} 
+                  onChange={this.handleChange} placeholder="User Name"/>
+               </div>
+               <div className="email">
+                  <label htmlFor="name">Email</label><br/>
+                  <input type="email" name="email" value={this.state.email}
+                   onChange={this.handleChange} placeholder="Example@email.com"/>
+               </div>
+               <div className="gen">
+                  <label htmlFor="name">Gen</label><br/>
+                  <input type="number" name="gen" value={this.state.gen}
+                   onChange={this.handleChange} placeholder="Gen"/>
+               </div>
+               <button type="submit">Save Changes</button>
+            </form>
+        );
+    }
+}
+
+const mapDispatchToProps = {
+     editUser:editUser
+}
+
+export default connect(null,mapDispatchToProps)(EditForm);

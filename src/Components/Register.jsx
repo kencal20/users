284f06840c25f690b40da5b81a8dce304f86eeea
kenneth adjei-mensah <kenadjei20@gmail.com>
2021-10-13 23:33:17 +0000

    register.jsx

---
+++ src/Components/Register.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { registerWithEmail } from '../actions/authActions';
+const Register = () => {
+    return (
+        <div>
+            <h1>Register</h1>
+            <form>
+            <div>
+            <label>Email</label>
+            <input name="email" type="email" placeholder="email"/>
+            </div>
+            <div>
+            <label>Password</label>
+            <input name="password" type="password" placeholder="password"/>
+            </div>
+            <hr/>
+            <button type="submit">Submit</button>
+            </form>
+        </div>
+    );
+}
+const mapStatetoprops= ()=>{
+}
+const mapDispatchToProps= {registerWithEmail}
+
+export default connect(mapStatetoprops,mapDispatchToProps)(Register)

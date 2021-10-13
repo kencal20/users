19b25f20756971be295c9ea8de5d983ae15aaab5
kenneth adjei-mensah <kenadjei20@gmail.com>
2021-10-13 23:32:58 +0000

    auth actions

---
+++ src/actions/authActions.js
@@ -0,0 +1,16 @@
+export const registerWithEmail = (email, password) => {
+  return (dispatch, state, { getFirebase }) =>
+    firebase
+    .auth()
+    .createUserWithEmailAndPassword(email, password)
+    .then((userCredential) => {
+      
+      const user = userCredential.user;
+      
+    })
+    .catch((error) => {
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      // ..
+    });
+  }

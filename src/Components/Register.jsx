945e9a7fdc2936bc91bac04c1124680263081c87
kenneth adjei-mensah <kenadjei20@gmail.com>
2021-10-15 11:12:14 +0000

    register jsx

--- src/Components/Register.jsx
+++ src/Components/Register.jsx
@@ -1,27 +1,26 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import { registerWithEmail } from '../actions/authActions';
+import React from "react";
+import { connect } from "react-redux";
+import { registerWithEmail } from "../actions/authActions";
 const Register = () => {
-    return (
+  return (
+    <div>
+      <h1>Register</h1>
+      <form>
         <div>
-            <h1>Register</h1>
-            <form>
-            <div>
-            <label>Email</label>
-            <input name="email" type="email" placeholder="email"/>
-            </div>
-            <div>
-            <label>Password</label>
-            <input name="password" type="password" placeholder="password"/>
-            </div>
-            <hr/>
-            <button type="submit">Submit</button>
-            </form>
+          <label>Email</label>
+          <input name="email" type="email" placeholder="email" />
         </div>
-    );
-}
-const mapStatetoprops= ()=>{
-}
-const mapDispatchToProps= {registerWithEmail}
+        <div>
+          <label>Password</label>
+          <input name="password" type="password" placeholder="password" />
+        </div>
+        <hr />
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+};
+const mapStatetoprops = () => {};
+const mapDispatchToProps = { registerWithEmail };
 
-export default connect(mapStatetoprops,mapDispatchToProps)(Register)
+export default connect(mapStatetoprops, mapDispatchToProps)(Register);
